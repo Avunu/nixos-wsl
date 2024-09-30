44565ce38f7c1b5e494e9365b9b8406b5553c706
Avunu LLC <mail@avu.nu>
2024-09-30 18:35:55 +0000

    nixfmt

--- flake.nix
+++ flake.nix
@@ -5,68 +5,82 @@
     vscode-server.url = "github:nix-community/nixos-vscode-server";
   };
 
-  outputs = { self, nixpkgs, nixos-wsl, vscode-server, ... }: {
-    nixosConfigurations = {
-      nixos = nixpkgs.lib.nixosSystem {
-        system = "x86_64-linux";
-        modules = [
-          
-          nixos-wsl.nixosModules.default
+  outputs =
+    {
+      self,
+      nixpkgs,
+      nixos-wsl,
+      vscode-server,
+      ...
+    }:
+    {
+      nixosConfigurations = {
+        nixos = nixpkgs.lib.nixosSystem {
+          system = "x86_64-linux";
+          modules = [
 
-          vscode-server.nixosModules.default
-          
-          ({ pkgs, ... }: {
+            nixos-wsl.nixosModules.default
 
-            environment.systemPackages = with pkgs; [
-                bun
-                curl
-                git
-                nano
-                nixfmt-rfc-style
-                nodejs_22
-                nodePackages.wrangler
-                pnpm
-                wget
-                yarn
-            ];
+            vscode-server.nixosModules.default
 
-            nix.settings.experimental-features = [ "nix-command" "flakes" ];
+            (
+              { pkgs, ... }:
+              {
 
-            programs.nix-ld = {
-                enable = true;
-                package = pkgs.nix-ld-rs;
-            };
-
-            services.vscode-server.enable = true;
-
-            system = {
-              stateVersion = "24.05";
-              autoUpgrade = {
-                enable = true;
-                allowReboot = false;
-                dates = "daily";
-                flake = "github:Avunu/nixos-wsl";
-                flags = [
-                  "--update-input"
-                  "nixpkgs"
-                  "-L"  # print build logs
+                environment.systemPackages = with pkgs; [
+                  bun
+                  curl
+                  git
+                  nano
+                  nixfmt-rfc-style
+                  nodejs_22
+                  nodePackages.wrangler
+                  pnpm
+                  wget
+                  yarn
                 ];
-              };
-            };
-          
-            wsl = {
-              enable = true;
-              defaultUser = "nixos";
-              extraBin = with pkgs; [
-                {src = "${uutils-coreutils-noprefix}/bin/cat";}
-                {src = "${uutils-coreutils-noprefix}/bin/whoami";}
-                {src = "${busybox}/bin/addgroup";}
-                {src = "${su}/bin/groupadd";}
-              ];
-            };
-          })
-        ];
+
+                nix.settings.experimental-features = [
+                  "nix-command"
+                  "flakes"
+                ];
+
+                programs.nix-ld = {
+                  enable = true;
+                  package = pkgs.nix-ld-rs;
+                };
+
+                services.vscode-server.enable = true;
+
+                system = {
+                  stateVersion = "24.05";
+                  autoUpgrade = {
+                    enable = true;
+                    allowReboot = false;
+                    dates = "daily";
+                    flake = "github:Avunu/nixos-wsl";
+                    flags = [
+                      "--update-input"
+                      "nixpkgs"
+                      "-L" # print build logs
+                    ];
+                  };
+                };
+
+                wsl = {
+                  enable = true;
+                  defaultUser = "nixos";
+                  extraBin = with pkgs; [
+                    { src = "${uutils-coreutils-noprefix}/bin/cat"; }
+                    { src = "${uutils-coreutils-noprefix}/bin/whoami"; }
+                    { src = "${busybox}/bin/addgroup"; }
+                    { src = "${su}/bin/groupadd"; }
+                  ];
+                };
+              }
+            )
+          ];
+        };
       };
     };
-  };
 }
