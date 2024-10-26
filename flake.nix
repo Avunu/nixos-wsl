09fde344204cb629bc84cb537c64b359427342f4
Kevin Shenk <mail@avu.nu>
2024-10-26 15:08:45 +0000

    enable podman on hyperv

--- flake.nix
+++ flake.nix
@@ -23,193 +23,217 @@
     }:
     let
       isWSL = builtins.pathExists /usr/lib/wsl/lib;
+
+      # WSL-specific module
+      wslModule =
+        { config, lib, ... }:
+        {
+          wsl = {
+            enable = true;
+            defaultUser = "nixos";
+            docker-desktop.enable = true;
+            nativeSystemd = true;
+            startMenuLaunchers = true;
+            useWindowsDriver = true;
+          };
+        };
+
+      # HyperV-specific module
+      hypervModule =
+        {
+          config,
+          pkgs,
+          lib,
+          ...
+        }:
+        {
+          boot = {
+            extraModulePackages = [ ];
+            initrd = {
+              availableKernelModules = [
+                "sd_mod"
+                "sr_mod"
+              ];
+              kernelModules = [ ];
+            };
+            kernelModules = [ ];
+            kernelPackages = pkgs.linuxPackages_latest;
+            loader = {
+              systemd-boot.enable = true;
+              efi.canTouchEfiVariables = true;
+            };
+          };
+
+          fileSystems = {
+            "/" = {
+              device = "/dev/disk/by-partlabel/root";
+              fsType = "btrfs";
+              options = [ "subvol=@" ];
+            };
+
+            "/boot" = {
+              device = "/dev/disk/by-partlabel/EFI";
+              fsType = "vfat";
+              options = [
+                "fmask=0077"
+                "dmask=0077"
+              ];
+            };
+          };
+
+          virtualisation = {
+            containers.storage.settings = {
+              storage = {
+                driver = "btrfs";
+                graphroot = "/var/lib/containers/storage";
+                runroot = "/run/containers/storage";
+              };
+            };
+            hypervGuest = {
+              enable = true;
+              videoMode = "1920x1080";
+            };
+            oci-containers.backend = "podman";
+            podman = {
+              autoPrune.enable = true;
+              dockerCompat = true;
+              dockerSocket.enable = true;
+              enable = true;
+              networkSocket.enable = true;
+            };
+          };
+
+          networking = {
+            useDHCP = true;
+            interfaces = {
+              eth0.useDHCP = true;
+            };
+          };
+        };
+
+      # Common configuration module
+      commonModule =
+        {
+          config,
+          pkgs,
+          lib,
+          ...
+        }:
+        let
+          pythonPackages = pkgs.python3.withPackages (
+            python-pkgs: with python-pkgs; [
+              black
+              flake8
+              isort
+              pandas
+              requests
+            ]
+          );
+        in
+        {
+          environment = {
+            systemPackages = with pkgs; [
+              bun
+              ccache
+              curl
+              git
+              nano
+              nixfmt-rfc-style
+              nixos-container
+              nixpkgs-fmt
+              nodejs_22
+              nodePackages.wrangler
+              pnpm
+              pythonPackages
+              tzdata
+              wget
+              yarn
+              zed-editor
+              attic.packages.${pkgs.system}.attic
+            ];
+          };
+
+          hardware.graphics = {
+            enable = true;
+            extraPackages = with pkgs; [
+              mesa.drivers
+              libvdpau-va-gl
+              vaapiVdpau
+            ];
+          };
+
+          nix.settings = {
+            experimental-features = [
+              "nix-command"
+              "flakes"
+            ];
+            extra-sandbox-paths = [ "/var/cache/ccache" ];
+            substituters = [
+              "https://attic.batonac.com/k3s"
+            ];
+            trusted-public-keys = [
+              "k3s:A8GYNJNy2p/ZMtxVlKuy1nZ8bnZ84PVfqPO6kg6A6qY="
+            ];
+            trusted-users = [
+              "root"
+              "nixos"
+              "@wheel"
+            ];
+          };
+
+          programs = {
+            ccache = {
+              cacheDir = "/var/cache/ccache";
+              enable = true;
+            };
+            direnv.enable = true;
+          };
+
+          services = {
+            openssh.enable = true;
+            vscode-server.enable = true;
+          };
+
+          system = {
+            stateVersion = "24.11";
+            autoUpgrade = {
+              enable = true;
+              allowReboot = false;
+              dates = "daily";
+              flake = "github:Avunu/nixos-wsl";
+              flags = [
+                "--update-input"
+                "nixpkgs"
+                "--refresh"
+                "--impure"
+              ];
+            };
+          };
+
+          users = {
+            users.nixos = {
+              isNormalUser = true;
+              extraGroups = [
+                "docker"
+                "libvirtd"
+                "nixbld"
+                "wheel"
+              ];
+              shell = pkgs.bashInteractive;
+            };
+          };
+        };
     in
     {
-
       nixosConfigurations = {
         nixos = nixpkgs.lib.nixosSystem {
           system = "x86_64-linux";
           modules = [
-
             nixos-wsl.nixosModules.default
-
             vscode-server.nixosModules.default
-
-            (
-              {
-                config,
-                pkgs,
-                lib,
-                ...
-              }:
-              let
-                pythonPackages = pkgs.python3.withPackages (
-                  python-pkgs: with python-pkgs; [
-                    black
-                    flake8
-                    isort
-                    pandas
-                    requests
-                  ]
-                );
-              in
-              {
-                # HyperV-specific configuration
-                boot = lib.mkIf (!isWSL) {
-                  extraModulePackages = [ ];
-                  initrd = {
-                    availableKernelModules = [
-                      "sd_mod"
-                      "sr_mod"
-                    ];
-                    kernelModules = [ ];
-                  };
-                  kernelModules = [ ];
-                  kernelPackages = pkgs.linuxPackages_latest;
-                  loader = {
-                    systemd-boot.enable = true;
-                    efi.canTouchEfiVariables = true;
-                  };
-                };
-
-                environment = {
-                  systemPackages = with pkgs; [
-                    bun
-                    ccache
-                    curl
-                    git
-                    nano
-                    nixfmt-rfc-style
-                    nixos-container
-                    nixpkgs-fmt
-                    nodejs_22
-                    nodePackages.wrangler
-                    pnpm
-                    pythonPackages
-                    tzdata
-                    wget
-                    yarn
-                    zed-editor
-                    attic.packages.${pkgs.system}.attic
-                  ];
-                };
-
-                # HyperV-specific configuration
-                fileSystems = lib.mkIf (!isWSL) {
-                  "/" = {
-                    device = "/dev/disk/by-partlabel/root";
-                    fsType = "btrfs";
-                    options = [ "subvol=@" ];
-                  };
-
-                  "/boot" = {
-                    device = "/dev/disk/by-partlabel/EFI";
-                    fsType = "vfat";
-                    options = [
-                      "fmask=0077"
-                      "dmask=0077"
-                    ];
-                  };
-                };
-
-                hardware.graphics = {
-                  enable = true;
-
-                  extraPackages = with pkgs; [
-                    mesa.drivers
-                    libvdpau-va-gl
-                    vaapiVdpau
-                  ];
-                };
-
-                nix.settings = {
-                  experimental-features = [
-                    "nix-command"
-                    "flakes"
-                  ];
-                  extra-sandbox-paths = [ "/var/cache/ccache" ];
-                  substituters = [
-                    "https://attic.batonac.com/k3s"
-                  ];
-                  trusted-public-keys = [
-                    "k3s:A8GYNJNy2p/ZMtxVlKuy1nZ8bnZ84PVfqPO6kg6A6qY="
-                  ];
-                  trusted-users = [
-                    "root"
-                    "nixos"
-                    "@wheel"
-                  ];
-                };
-
-                programs = {
-                  ccache = {
-                    cacheDir = "/var/cache/ccache";
-                    enable = true;
-                  };
-                  direnv.enable = true;
-                };
-
-                services = {
-                  openssh.enable = true;
-                  vscode-server.enable = true;
-                };
-
-                system = {
-                  stateVersion = "24.11";
-                  autoUpgrade = {
-                    enable = true;
-                    allowReboot = false;
-                    dates = "daily";
-                    flake = "github:Avunu/nixos-wsl";
-                    flags = [
-                      "--update-input"
-                      "nixpkgs"
-                      "--refresh"
-                    ];
-                  };
-                };
-
-                users = {
-                  users.nixos = {
-                    isNormalUser = true;
-                    extraGroups = [
-                      "docker"
-                      "libvirtd"
-                      "nixbld"
-                      "wheel"
-                    ];
-                    shell = pkgs.bashInteractive;
-                  };
-                };
-
-                # WSL-specific configuration
-                wsl = lib.mkIf (isWSL) {
-                  enable = true;
-                  defaultUser = "nixos";
-                  docker-desktop.enable = true;
-                  nativeSystemd = true;
-                  startMenuLaunchers = true;
-                  useWindowsDriver = true;
-                };
-
-                # HyperV-specific configuration
-                virtualisation = lib.mkIf (!isWSL) {
-                  hypervGuest = {
-                    enable = true;
-                    videoMode = "1920x1080";
-                  };
-                };
-
-                # Optional: HyperV-specific networking
-                networking = lib.mkIf (!isWSL) {
-                  useDHCP = true; # Or configure static IP if needed
-                  # Enable specific network interfaces if needed
-                  interfaces = {
-                    eth0.useDHCP = true;
-                  };
-                };
-              }
-            )
+            commonModule
+            (lib.mkIf isWSL wslModule)
+            (lib.mkIf (!isWSL) hypervModule)
           ];
         };
       };
